67b2bcac27573c24c9c4bcf3ad36b7528c61498e
JaviSrz <ljsuarez7@gmail.com>
2024-10-21 23:38:57 +0200

    acciones de las teclas y calculo

--- src/app/calculator/components/calculator/calculator.component.ts
+++ src/app/calculator/components/calculator/calculator.component.ts
@@ -1,5 +1,6 @@
-import { ChangeDetectionStrategy, Component, viewChildren } from '@angular/core';
+import { ChangeDetectionStrategy, Component, computed, inject, viewChildren } from '@angular/core';
 import { CalculatorButtonComponent } from '../calculator-button/calculator-button.component';
+import { CalculatorService } from '@/calculator/services/calculator.service';
 
 @Component({
   selector: 'calculator',
@@ -15,10 +16,17 @@ import { CalculatorButtonComponent } from '../calculator-button/calculator-butto
 })
 export class CalculatorComponent {
 
+  private calculatorService = inject(CalculatorService);
+
   public calculatorButtons = viewChildren(CalculatorButtonComponent);
 
+  public resultText = computed(() => this.calculatorService.resultText());
+  public subResultText = computed(() => this.calculatorService.subResultText());
+  public lastOperator = computed(() => this.calculatorService.lastOperator());
+
   handleClick(key: string) {
-    console.log({ key });
+    // console.log({ key });
+    this.calculatorService.constructNumber(key);
   }
 
   // @HostListener('document:keyup', ['$event'])
@@ -27,7 +35,7 @@ export class CalculatorComponent {
     const keyEquivalents: Record<string, string> = {
       Escape: 'C',
       Clear: 'C',
-      '*': 'x',
+      'x': '*',
       '/': '÷',
       Enter: '='
     }
